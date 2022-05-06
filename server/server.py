a0fe35b5ee2ab913bd91b302be2cdaf98f881665
Huamin Chen <hchen@redhat.com>
2022-05-06 15:37:40 -0400

    add model server skeleton

---
+++ server/server.py
@@ -0,0 +1,34 @@
+from flask import Flask, redirect, url_for, request, json
+app = Flask(__name__)
+
+@app.route('/model',methods = ['POST', 'GET'])
+def model():
+   if request.method == 'GET':
+      ret = makeCoeff()
+      app.logger.warn("BODY: %s" % ret)
+      return ret
+
+@app.route('/data',methods = ['POST', 'GET'])
+def data():
+   if request.method == 'POST':
+      app.logger.warn("BODY: %s" % request.get_data())
+      return 'success'
+
+
+def makeCoeff():
+    data = {
+      "cpu_time": 0.6,
+      "cpu_cycles": 0.2,
+      "cpu_instructions": 0.2,
+      "memory_usage": 0.5,
+      "cache_misses": 0.5,
+    }
+    response = app.response_class(
+        response=json.dumps(data),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
+
+if __name__ == '__main__':
+   app.run(debug = True)
