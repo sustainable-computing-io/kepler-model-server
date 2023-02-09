85db8ab644e4270ebedf52c6af2762777be598e0
Sunyanan Choochotkaew <sunyanan.choochotkaew1@ibm.com>
2023-02-09 13:35:53 +0900

    update prom to one metric per query
    Signed-off-by: Sunyanan Choochotkaew <sunyanan.choochotkaew1@ibm.com>

---
+++ tests/prom_test.py
@@ -0,0 +1,25 @@
+# prom_test.py
+#   call prometheus module to query data save to query_data/<export_metric>.csv
+
+import os
+import sys
+
+server_path = os.path.join(os.path.dirname(__file__), '../server')
+util_path = os.path.join(os.path.dirname(__file__), '../server/util')
+prom_path = os.path.join(os.path.dirname(__file__), '../server/prom')
+
+sys.path.append(server_path)
+sys.path.append(util_path)
+sys.path.append(prom_path)
+
+prom_output_path = os.path.join(os.path.dirname(__file__), 'data', 'prom_output')
+
+from prom.query import PrometheusClient
+
+if __name__ == '__main__':
+    prom_client = PrometheusClient()
+    prom_client.query()
+    results = prom_client.snapshot_query_result()
+    # save query data in csv
+    for metric, data in results.items():
+        data.to_csv("{}/{}.csv".format(prom_output_path, metric))
