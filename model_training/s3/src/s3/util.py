6cbbf93dea9fc656780d2872d941b083bc0954d7
Sunil Thaha <sthaha@redhat.com>
2024-07-08 19:04:00 +1000

    chore(s3): add --version arg
    Signed-off-by: Sunil Thaha <sthaha@redhat.com>

--- model_training/s3/src/s3/util.py
+++ model_training/s3/src/s3/util.py
@@ -1,22 +1,26 @@
+import argparse
+import s3.__about__ as about
+
+
 def new_ibm_client(args):
     import ibm_boto3
     from ibm_botocore.client import Config
-    cos = ibm_boto3.resource('s3',
-        ibm_api_key_id=args.api_key,
-        ibm_service_instance_id=args.service_instance_id,
-        config=Config(signature_version='oauth'),
-        endpoint_url=args.service_endpoint
-    )
+
+    cos = ibm_boto3.resource("s3", ibm_api_key_id=args.api_key, ibm_service_instance_id=args.service_instance_id, config=Config(signature_version="oauth"), endpoint_url=args.service_endpoint)
     return cos
-    
+
+
 def new_aws_client(args):
     import boto3 as aws_boto3
-    s3 = aws_boto3.client('s3', aws_access_key_id=args.aws_access_key_id, aws_secret_access_key=args.aws_secret_access_key, region_name=args.region_name)
+
+    s3 = aws_boto3.client("s3", aws_access_key_id=args.aws_access_key_id, aws_secret_access_key=args.aws_secret_access_key, region_name=args.region_name)
     return s3
 
-def get_command(parser, add_common_args, ibm_func, aws_func):
-    subparsers = parser.add_subparsers(title="S3 provider", dest="provider")
 
+def get_command(parser: argparse.ArgumentParser, add_common_args, ibm_func, aws_func):
+    parser.add_argument("--version", action="version", version=about.__version__)
+
+    subparsers = parser.add_subparsers(title="S3 provider", dest="provider")
     ibm_parser = subparsers.add_parser("ibmcloud", help="IBM Cloud")
     ibm_parser.add_argument("--api-key", type=str, help="API key", required=True)
     ibm_parser.add_argument("--service-instance-id", type=str, help="Service instance ID", required=True)
