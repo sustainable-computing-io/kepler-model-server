fix: mean calculation
Signed-off-by: Sunil Thaha <sthaha@redhat.com>

src/train/extractor/extractor.py
@@ -1,20 +1,13 @@
-import os
-import sys
 import pandas as pd
 import numpy as np
 from abc import ABCMeta, abstractmethod
 
-util_path = os.path.join(os.path.dirname(__file__), '..', '..', 'util')
-sys.path.append(util_path)
+from util.train_types import FeatureGroups, FeatureGroup, SYSTEM_FEATURES
+from util.prom_types import TIMESTAMP_COL, SOURCE_COL, get_energy_unit, usage_ratio_query, node_info_query, energy_component_to_query, feature_to_query, pkg_id_column, container_id_cols, node_info_column
+from util.loader import default_node_type
+from util.extract_types import container_id_colname, ratio_to_col, component_to_col, get_unit_vals, accelerator_type_colname
+from train.extractor.preprocess import drop_zero_column, find_correlations
 
-from train_types import FeatureGroups, FeatureGroup, SYSTEM_FEATURES
-from prom_types import TIMESTAMP_COL, SOURCE_COL, get_energy_unit, \
-    usage_ratio_query,node_info_query, \
-        energy_component_to_query, feature_to_query, \
-            pkg_id_column, container_id_cols, node_info_column
-from loader import default_node_type
-from extract_types import container_id_colname, ratio_to_col, component_to_col, get_unit_vals, accelerator_type_colname
-from preprocess import drop_zero_column, find_correlations
 
 # append ratio for each unit
 def append_ratio_for_pkg(feature_power_data, is_aggr, query_results, power_columns):
@@ -31,7 +24,7 @@ def append_ratio_for_pkg(feature_power_data, is_aggr, query_results, power_colum
         if is_aggr:
             ratio_df = ratio_df.groupby([TIMESTAMP_COL, pkg_id_column]).sum()[usage_ratio_query]
         else:
-            ratio_df[container_id_colname] = ratio_df[container_id_cols].apply(lambda x: '/'.join(x), axis=1)
+            ratio_df[container_id_colname] = ratio_df[container_id_cols].apply(lambda x: "/".join(x), axis=1)
             ratio_df = ratio_df.groupby([TIMESTAMP_COL, pkg_id_column, container_id_colname]).sum()[usage_ratio_query]   
     ratio_colnames = []
     for unit_val in unit_vals:
@@ -43,11 +36,12 @@ def append_ratio_for_pkg(feature_power_data, is_aggr, query_results, power_colum
             feature_power_data = feature_power_data.join(target_ratio_df).dropna()
             feature_power_data = feature_power_data.rename(columns={usage_ratio_query: ratio_colname})
         ratio_colnames +=[ratio_colname]
-    tmp_total_col = 'total_ratio'
+    tmp_total_col = "total_ratio"
     feature_power_data[tmp_total_col] = feature_power_data[ratio_colnames].sum(axis=1)
     for ratio_colname in ratio_colnames:
         feature_power_data[ratio_colname] /= feature_power_data[tmp_total_col]
     return feature_power_data.drop(columns=[tmp_total_col])
+
 
 class Extractor(metaclass=ABCMeta):
     # extractor abstract:
@@ -65,12 +59,13 @@ class Extractor(metaclass=ABCMeta):
     def get_name(self):
         return NotImplemented
 
+
 # extract data from query 
 # for node-level
 # return DataFrame (index=timestamp, column=[features][power columns][node_type]), power_columns
 
-class DefaultExtractor(Extractor):
 
+class DefaultExtractor(Extractor):
     def get_name(self):
         return "default"
 
@@ -103,8 +98,8 @@ class DefaultExtractor(Extractor):
         is_aggr = node_level and aggr
         if is_aggr:
             # sum stat of all containers
-            sum_feature = feature_power_data.groupby([TIMESTAMP_COL]).sum()[workload_features]
-            mean_power = feature_power_data.groupby([TIMESTAMP_COL]).mean()[power_columns]
+            sum_feature = feature_power_data.groupby([TIMESTAMP_COL])[workload_features].sum()
+            mean_power = feature_power_data.groupby([TIMESTAMP_COL])[power_columns].mean()
             feature_power_data = sum_feature.join(mean_power)
         else:
             feature_power_data = feature_power_data.groupby([TIMESTAMP_COL, container_id_colname]).sum()
@@ -148,7 +143,7 @@ class DefaultExtractor(Extractor):
             
             if all(col in aggr_query_data.columns for col in container_id_cols):
                 aggr_query_data.rename(columns={query: feature}, inplace=True)
-                aggr_query_data[container_id_colname] = aggr_query_data[container_id_cols].apply(lambda x: '/'.join([str(xi) for xi in x]), axis=1)
+                aggr_query_data[container_id_colname] = aggr_query_data[container_id_cols].apply(lambda x: "/".join([str(xi) for xi in x]), axis=1)
                 # separate for each container_id
                 container_id_list = pd.unique(aggr_query_data[container_id_colname])
 
@@ -222,7 +217,7 @@ class DefaultExtractor(Extractor):
             unit_col = get_energy_unit(component) # such as package
             query = energy_component_to_query(component)
             if query not in query_results:
-                print(query, 'not in', query_results)
+                print(query, "not in", query_results)
                 return None
             aggr_query_data = query_results[query].copy()
             # filter source

src/train/isolator/isolator.py
@@ -1,18 +1,10 @@
-import os
-import sys
 import pandas as pd
 from abc import ABCMeta, abstractmethod
 
-util_path = os.path.join(os.path.dirname(__file__), '..', '..', 'util')
-sys.path.append(util_path)
 
-estimate_path = os.path.join(os.path.dirname(__file__), '..', '..', 'estimate')
-sys.path.append(estimate_path)
-
-
-from train_types import PowerSourceMap
-from extract_types import container_id_colname, col_to_component, get_num_of_unit, all_container_key
-from prom_types import TIMESTAMP_COL, node_info_column, get_container_name_from_id
+from util.train_types import PowerSourceMap
+from util.extract_types import container_id_colname, col_to_component, get_num_of_unit, all_container_key
+from util.prom_types import TIMESTAMP_COL, node_info_column, get_container_name_from_id
 
 from estimate import get_background_containers, get_predicted_power_colname, get_predicted_background_power_colname, get_predicted_dynamic_background_power_colname, get_label_power_colname, get_reconstructed_power_colname
 
@@ -35,12 +27,14 @@ class Isolator(metaclass=ABCMeta):
     def get_name(self):
         return NotImplemented
 
+
 def exclude_target_container_usage(data, target_container_id):
     target_container_data = data[data[container_id_colname]==target_container_id]
-    filled_target_container_data = data[target_container_data.columns].join(target_container_data, lsuffix='_target').fillna(0)
-    filled_target_container_data.drop(columns=[col for col in filled_target_container_data.columns if '_target' in col], inplace=True)
+    filled_target_container_data = data[target_container_data.columns].join(target_container_data, lsuffix="_target").fillna(0)
+    filled_target_container_data.drop(columns=[col for col in filled_target_container_data.columns if "_target" in col], inplace=True)
     conditional_data = data - filled_target_container_data
     return target_container_data, conditional_data
+
 
 # target_containers are containers that are not in idle
 # idle_data in json format
@@ -48,6 +42,7 @@ def get_target_containers(data, background_containers):
     container_ids = pd.unique(data[container_id_colname])
     target_containers = [container_id for container_id in container_ids if get_container_name_from_id(container_id) not in background_containers]
     return target_containers, background_containers
+
 
 # isolate_container
 def isolate_container(extracted_data, background_containers, label_cols):
@@ -71,15 +66,16 @@ def squeeze_data(container_level_data, label_cols):
     groupped_sum_data = container_level_data.groupby([TIMESTAMP_COL]).sum()[ratio_columns + feature_columns]
     # re-normalize ratio column
     # each ratio / total ratio for all energy component
-    groupped_sum_data['sum_ratio'] = groupped_sum_data[ratio_columns].sum(axis=1)
+    groupped_sum_data["sum_ratio"] = groupped_sum_data[ratio_columns].sum(axis=1)
     for ratio_col in ratio_columns:
-        groupped_sum_data[ratio_col] /= groupped_sum_data['sum_ratio']
-    groupped_sum_data = groupped_sum_data.drop(columns=['sum_ratio'])
+        groupped_sum_data[ratio_col] /= groupped_sum_data["sum_ratio"]
+    groupped_sum_data = groupped_sum_data.drop(columns=["sum_ratio"])
     # use mean value for node-level information
     groupped_mean_data = container_level_data.groupby([TIMESTAMP_COL]).mean()[node_level_columns]  
     squeeze_data = groupped_sum_data.join(groupped_mean_data)
     squeeze_data[container_id_colname] = all_container_key
     return squeeze_data.reset_index()
+
 
 class MinIdleIsolator(Isolator):
     def isolate(self, data, label_cols, energy_source=None):
@@ -104,9 +100,11 @@ class MinIdleIsolator(Isolator):
     def get_name(self):
         return "min"
     
+
 import numpy as np
 
 system_process_id = "system_processes/system_processes/system"
+
 
 class ProfileBackgroundIsolator(Isolator):
     def __init__(self, profiles, idle_data):
@@ -133,11 +131,11 @@ class ProfileBackgroundIsolator(Isolator):
         try:
             for label_col in label_cols:
                 component = col_to_component(label_col)
-                isolated_data['profile'] = isolated_data[node_info_column].transform(self.transform_profile, energy_source=energy_source, component=component)
-                if isolated_data['profile'].isnull().values.any():
+                isolated_data["profile"] = isolated_data[node_info_column].transform(self.transform_profile, energy_source=energy_source, component=component)
+                if isolated_data["profile"].isnull().values.any():
                     return None
-                isolated_data[label_col] = isolated_data[label_col] - isolated_data['profile']
-                isolated_data.drop(columns='profile', inplace=True)
+                isolated_data[label_col] = isolated_data[label_col] - isolated_data["profile"]
+                isolated_data.drop(columns="profile", inplace=True)
             if index_list[0] is not None:
                 isolated_data = isolated_data.set_index(index_list)
             return isolated_data
@@ -152,12 +150,12 @@ class ProfileBackgroundIsolator(Isolator):
         copy_data = extracted_data.copy()
         for energy_component in energy_components:
             try:
-                copy_data['profile'] = copy_data[node_info_column].transform(self.transform_profile, energy_source=energy_source, component=energy_component)
-                if copy_data['profile'].isnull().values.any():
+                copy_data["profile"] = copy_data[node_info_column].transform(self.transform_profile, energy_source=energy_source, component=energy_component)
+                if copy_data["profile"].isnull().values.any():
                     return None       
                 predicted_colname = get_predicted_power_colname[energy_source]
                 background_power_colname = get_predicted_background_power_colname(energy_component)
-                reconstructed_data[background_power_colname] = (copy_data['profile'] * num_of_unit)
+                reconstructed_data[background_power_colname] = copy_data["profile"] * num_of_unit
                 reconstructed_data[get_reconstructed_power_colname(energy_component)] = data_with_prediction[predicted_colname]  + reconstructed_data[background_power_colname] 
             except Exception as e:
                 print(e)
@@ -167,9 +165,9 @@ class ProfileBackgroundIsolator(Isolator):
     def get_name(self):
         return "profile"
 
+
 # no isolation
 class NoneIsolator(Isolator):
-
     def isolate(self, data, label_cols, energy_source=None):
         isolated_data = squeeze_data(data, label_cols)
         return isolated_data
@@ -177,6 +175,6 @@ class NoneIsolator(Isolator):
     def reconstruct(self, extracted_data, data_with_prediction, energy_source, label_cols):
         return data_with_prediction
     
-
     def get_name(self):
         return "none"
+