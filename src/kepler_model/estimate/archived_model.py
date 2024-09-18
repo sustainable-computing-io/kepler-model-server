7b5f3dc739ee06e6e01af8c4d705775f72cf0058
Sunil Thaha <sthaha@redhat.com>
2024-09-18 23:17:07 +1000

    chore: use logger instead of print
    Signed-off-by: Sunil Thaha <sthaha@redhat.com>

--- src/kepler_model/estimate/archived_model.py
+++ src/kepler_model/estimate/archived_model.py
@@ -1,3 +1,4 @@
+import logging
 import requests
 from requests_file import FileAdapter
 
@@ -5,6 +6,8 @@ from kepler_model.estimate.model_server_connector import unpack
 from kepler_model.util.config import get_init_model_url
 from kepler_model.util.loader import load_metadata
 from kepler_model.util.train_types import ModelOutputType
+
+logger = logging.getLogger(__name__)
 
 failed_list = []
 
@@ -39,19 +42,21 @@ def valid_metrics(metrics, features):
 def is_valid_model(metrics, metadata, filters):
     if not valid_metrics(metrics, metadata["features"]):
         return False
+
     for attrb, val in filters.items():
         if not hasattr(metadata, attrb) or getattr(metadata, attrb) is None:
-            print("{} has no {}".format(metadata["model_name"], attrb))
+            logger.warn(f"{metadata['model_name']} has no {attrb}")
             return False
-        else:
-            cmp_val = getattr(metadata, attrb)
-            val = float(val)
-            if attrb == "abs_max_corr":  # higher is better
-                valid = cmp_val >= val
-            else:  # lower is better
-                valid = cmp_val <= val
-            if not valid:
-                return False
+
+        cmp_val = getattr(metadata, attrb)
+        val = float(val)
+        if attrb == "abs_max_corr":  # higher is better
+            valid = cmp_val >= val
+        else:  # lower is better
+            valid = cmp_val <= val
+        if not valid:
+            return False
+
     return True
 
 
@@ -61,7 +66,6 @@ def reset_failed_list():
 
 
 def get_achived_model(power_request):
-    print("get archived model")
     global failed_list
     output_type_name = power_request.output_type
     if output_type_name in failed_list:
@@ -69,7 +73,7 @@ def get_achived_model(power_request):
     output_type = ModelOutputType[power_request.output_type]
     url = get_init_model_url(power_request.energy_source, output_type_name)
     if url == "":
-        print("no URL set for ", output_type_name, power_request.energy_source)
+        logger.warn("no URL set for ", output_type_name, power_request.energy_source)
         return None
     logger.info(f"try getting archieved model from URL: {url} for {output_type_name}")
 
@@ -89,7 +93,8 @@ def get_achived_model(power_request):
             if not is_valid_model(power_request.metrics, metadata, filters):
                 failed_list += [output_type_name]
                 return None
-        except:
-            print("cannot validate the archived model")
+        except Exception as e:
+            logger.warn(f"cannot validate the archived model: {e}")
             return None
+
     return output_path
